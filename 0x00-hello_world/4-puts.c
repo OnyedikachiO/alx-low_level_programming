6953fe1b6406748363011502468e1d90dbae764b
OnyedikachiO <Kachisichobernardokoh@gmail.com>
2023-02-19 13:36:38 +0000

    my 4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #!/bin/bash
- * Return: Always 0.
- *  */
- int main(void)
+* Return: Always 0.
+*/
+int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
